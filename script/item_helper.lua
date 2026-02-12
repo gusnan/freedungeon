3a66f4e9e95655591370e57e021f74390f36d86f
Andreas Rönnquist <andreas@ronnquist.net>
2026-02-12 16:27:06 +0100

    Add proper license header in lua files

--- script/item_helper.lua
+++ script/item_helper.lua
@@ -1,7 +1,7 @@
 --[[----------------------------------------------------------------------------
 
   This file is part of freedungeon
-  Copyright (C) 2017 Andreas Rönnquist
+  Copyright (C) 2017, 2026 Andreas Rönnquist
 
   freedungeon is free software: you can redistribute it and/or
   modify it under the terms of the GNU General Public License as published
