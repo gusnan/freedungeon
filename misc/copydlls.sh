9d76c31a536ac6f1370844179920d82c4d33bd21
Andreas Rönnquist <andreas@ronnquist.net>
2026-02-12 16:12:39 +0100

    move copydlls.sh to misc folder

---
+++ misc/copydlls.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This file is licensed under the GPL version 3, see GPL3.txt
+
+cp /mingw64/bin/allegro_primitives-5.2.dll ../build/
+cp /mingw64/bin/allegro-5.2.dll ../build/
+cp /mingw64/bin/allegro_acodec-5.2.dll ../build/
+cp /mingw64/bin/allegro_audio-5.2.dll ../build/
+cp /mingw64/bin/allegro_color-5.2.dll ../build/
+cp /mingw64/bin/allegro_dialog-5.2.dll ../build/
+cp /mingw64/bin/allegro_font-5.2.dll ../build/
+cp /mingw64/bin/allegro_image-5.2.dll ../build/
+cp /mingw64/bin/allegro_main-5.2.dll ../build/
+cp /mingw64/bin/allegro_memfile-5.2.dll ../build/
+cp /mingw64/bin/allegro_physfs-5.2.dll ../build/
+cp /mingw64/bin/allegro_ttf-5.2.dll ../build/
+cp /mingw64/bin/allegro_video-5.2.dll ../build/
+cp /mingw64/bin/libbrotlicommon.dll ../build/
+cp /mingw64/bin/libbrotlidec.dll ../build/
+cp /mingw64/bin/libbz2-1.dll ../build/
+cp /mingw64/bin/libfreetype-6.dll ../build/
+cp /mingw64/bin/libgcc_s_seh-1.dll ../build/
+cp /mingw64/bin/libglib-2.0-0.dll ../build/
+cp /mingw64/bin/libgraphite2.dll ../build/
+cp /mingw64/bin/libharfbuzz-0.dll ../build/
+cp /mingw64/bin/libiconv-2.dll ../build/
+cp /mingw64/bin/libintl-8.dll ../build/
+cp /mingw64/bin/libpcre-1.dll ../build/
+cp /mingw64/bin/libpcre2-16-0.dll ../build/
+cp /mingw64/bin/libpcre2-8-0.dll ../build/
+cp /mingw64/bin/libpng16-16.dll ../build/
+cp /mingw64/bin/libsharpyuv-0.dll ../build/
+cp /mingw64/bin/libstdc++-6.dll ../build/
+cp /mingw64/bin/libwebp-7.dll ../build/
+cp /mingw64/bin/libwinpthread-1.dll ../build/
